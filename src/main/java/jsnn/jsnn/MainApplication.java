54a0b4d51227b4c9321f8e77d42a9eab069d759b
Logan Neel <149398207+LoganNeel@users.noreply.github.com>
2025-06-24 19:58:13 -0700

    Change main

--- src/main/java/jsnn/jsnn/MainApplication.java
+++ src/main/java/jsnn/jsnn/MainApplication.java
@@ -6,12 +6,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.getStylesheets().add(Objects.requireNonNull(MainApplication.class.getResource("/styles/main.css")).toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
